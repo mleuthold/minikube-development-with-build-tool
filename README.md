78f2e136aa1bc71e92dc1811d205a5dd10218bf3
Martin Leuthold <martinleuthold88+git@gmail.com>
2018-11-12 00:21:33 +0100

    experiment with pylint and invoke to build software without a Makefile

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # minikube-development-with-build-tool
+
+```bash
+sudo apt-get install python3-pip
+pip3 install --user pipenv
+pip3 install --user invoke
+pip3 install --user pylint
+```
+
+```bash
+pylint tasks.py
+```
---
+++ tasks.py
@@ -0,0 +1,32 @@
+"""This files is the replacement of Makefile."""
+
+import shutil
+from invoke import task
+
+@task
+def clean(ctx, docs=False, bytecode=False, extra=''):
+    """Remove all created files."""
+    patterns = ['build']
+    if docs:
+        patterns.append('docs/_build')
+    if bytecode:
+        patterns.append('**/*.pyc')
+    if extra:
+        patterns.append(extra)
+    for pattern in patterns:
+        ctx.run("rm -rf {}".format(pattern))
+
+@task
+def build(ctx, docs=False):
+    """Build this software."""
+    ctx.run("python setup.py build")
+    if docs:
+        ctx.run("sphinx-build docs docs/_build")
+
+@task
+def check_dependencies():
+    """Checking all necessary binaries to build and deploy this software."""
+    dependencies = ['python3', 'minikube', 'docker', 'git', 'helm', 'kubectl', 'svn']
+    for dependency in dependencies:
+        if not shutil.which(dependency):
+            print("Please install %s" % dependency)
